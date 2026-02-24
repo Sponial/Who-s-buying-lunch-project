50462ebd29808037a4db70528f7bd9064247c582
Orson Stawiarski <orson@stazca.com>
2026-02-24 23:19:37 +0700

    Created a variable who's buying which will take the position from name list and then convert that into the name that is in that array position

--- WhosBuyingLunch.js
+++ WhosBuyingLunch.js
@@ -1,9 +1,13 @@
 var nameList = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
 function whosBuyingLunch() {
     var nameListPosition = nameList.length;
-    buyLunch = Math.random(nameListPosition);
-    buyLunch = buyLunch + " is going to buy lunch today!"
-    return buyLunch;
+    var buyLunch = Math.random(nameListPosition);
+    var whosBuying = nameList[buyLunch];
+    var message = whosBuying + " is going to buy lunch today!"
+    return message;
 }
 
 whosBuyingLunch();
+
+//Okay, so the code works, however it's printing the number rather than the string with the name, which means we need to get the positional correct as a start and we should probably create a separate variable which is called a message in hindsight
+//Also after working on that code, I needed to make sure that I have created vars for each of these different variables I was creating to use. 
